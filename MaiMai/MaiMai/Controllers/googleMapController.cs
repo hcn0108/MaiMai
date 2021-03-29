aeb2d92df40c562f1a3d72903d793538ba80b8c2
hcn0108 <77559249+hcn0108@users.noreply.github.com>
2021-03-29 22:00:49 +0800

    3/29

---
+++ MaiMai/MaiMai/Controllers/googleMapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaiMai.Controllers
+{
+    public class googleMapController : Controller
+    {
+        // GET: googleMap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
